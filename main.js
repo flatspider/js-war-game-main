f6cab8e12f3f17fa9acea16ee16a81e5561e41b8
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-11 14:52:07 -0500

    JS function commented in

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 // Scripting goes here
 
 console.log("Hello World");
+
+
+// Listen for the new game button. 
+
+// When new game button CLICKED, call the new game function. 
+
+// New game function will look at a deck of cards.
+
+// Shuffle the deck. 
+
+// Randomly assign (or alternately) assign 26 cards. 
+
+// Player 1 will now have 26 cards in their personal deck. 
+
+// Player 2 will now have 26 cards in their personal deck. 
+
+// BEGIN GAME.
+
+// Click the DRAW button. 
+
+// Player 1 contributes the first card from their deck. 
+
+// Player 2 contributes the first card from their deck. 
+
+// The computer evaluates player1.deck.card.value to player2.deck.card.value. 
+
+// Look at the symbol. Determine what value each card has. 
+
+// Directly compare the values. The higher value card wins. Aces are the highest card. 
+
+// If player 1 wins, take the two cards currently displayed and place them into Player 1s deck. 
+
+// If player 2 has the higher evaluated card, take the two cards currently evaluated and place them into Player 2 deck. 
+
+// First iteration: If the values are the same, Player 1 wins. 
+
+// This means deck array + current card1 and current card2. 
+
+// The game now stands at Player1 Deck = 28 and Player 2 deck = 24. 
+
+// Click the draw button and restart. 
