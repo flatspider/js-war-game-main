176ba7210b40bbb23ebd96e5696a9f29e1e5467e
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-12 14:20:19 -0500

    Single card added to deck

--- deck.js
+++ deck.js
@@ -1,8 +1,6 @@
 import { Card, CARD_RANK, SUIT_VALUE } from "./card.js";
 
 
-const newDeck = new Card();
-
 
 
 // Create 52 cards.
@@ -14,15 +12,42 @@ const newDeck = new Card();
 // Below will not output to the console because it is nested in the structure. 
 // Card is imported in here so we can use it to build the deck. 
 
+let rankQ = CARD_RANK[3];
+
+
+const firstCard = new Card({value: CARD_RANK[0], suit: SUIT_VALUE[3]});
+const secondCard = new Card({value: CARD_RANK[4], suit: SUIT_VALUE[2]});
+
+const simpleDeck = new Deck(); // This creates an empty deck.
+
+
+
+
+
+
+
+
+
+Card.prototype.addCard = function(deck) {
+    deck.how_many_cards_in_deck = deck.how_many_cards_in_deck + 1;
+}
+
+
+firstCard.addCard(simpleDeck);
+
+
+
+/*
 
 function createNewDeck(){
 
     const new_deck = new Deck();
+    let temp_card;
 
     for(let i = 0; i < SUIT_VALUE; i++) {
         for(let j = 0; j < CARD_RANK; j++){
             
-            let temp_card = new Card(SUIT_VALUE[i],CARD_RANK[j]);
+            temp_card = new Card({value: SUIT_VALUE[i], suit: CARD_RANK[j]}); //Updated pass in values to be an object.
             new_deck.how_many_cards_in_deck = new_deck.how_many_cards_in_deck + 1; //Increment the value
             new_deck.current_cards.push(temp_card); // This should be a method attached to the Deck prototype chain.
 
@@ -30,20 +55,21 @@ function createNewDeck(){
         }
 
     }
-return new_deck;
+
+return temp_card;
 }
 
-function Deck({num = 0, test = {}} = {}) {
+
+*/ 
+
+
+
+function Deck({num = 0, test = []} = {}) {
     this.how_many_cards_in_deck = num;
-    this.current_cards = {};
+    this.current_cards = test;
 }
 
 
 
-
-
-
-
-
-export {createNewDeck};
-export {newDeck};
+//export {createNewDeck};
+export {firstCard, secondCard, simpleDeck};
--- new-main.js
+++ new-main.js
@@ -11,7 +11,10 @@ import * as cardDeck from  "./deck.js";
 // Not sure if everything should be imported into this new-main file, or if I can reach back.
 console.log("hello TIME");
 //cardDeck.newDeck.value = 3; //This updates the value of the card.
-console.log(cardDeck.createNewDeck()); // No parenthesis. Have to put parenthesis to call the function. 
-//console.log(cardDeck.newDeck);
+//console.log(cardDeck.createNewDeck()); // No parenthesis. Have to put parenthesis to call the function. 
+console.log(cardDeck.firstCard);
+console.log(cardDeck.secondCard);
+console.log(cardDeck.simpleDeck.how_many_cards_in_deck);
+
 
 
